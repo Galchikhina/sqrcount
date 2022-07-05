e926e25a212539d19f66bdc06055a7654f326a24
ElenaG <lenok1586@mail.ru>
2022-07-05 18:08:35 +0300

    update Parameters name

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,10 +1,10 @@
 package ru.netology.sqr;
 
 public class SQRService {
-    public int calcNumberSqr(int a, int b) {
+    public int calcNumberSqr(int minRangeFigure, int maxRangeFigure) {
         int count = 0;
         for (int i = 10; i <= 99; i++) {
-            if (i * i >= a && i * i <= b)
+            if (i * i >= minRangeFigure && i * i <= maxRangeFigure)
                 count++;
         }
         return count;
